2781bf1b13b0bc290a4af5985fe5f4339b031150
FrancescoRisso <francesco@frisso.net>
2024-01-11 11:03:25 +0100

    Fixed RNG excluding the max value

--- Game/AI/AI.c
+++ Game/AI/AI.c
@@ -4,8 +4,8 @@
 int AI_random(int max) {
 	int i, sum = 0;
 
-	for(i = 0; i < randomIterations; i++) sum += LPC_RIT->RICOUNTER % max;
-	return sum % max;
+	for(i = 0; i < randomIterations; i++) sum += LPC_RIT->RICOUNTER % (max + 1);
+	return sum % (max + 1);
 }
 
 
